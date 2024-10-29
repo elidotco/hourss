6b6d6d3baee55fcde8d56a46581861dbd5a2e964
Ebenezer Ametepeh <eddiefestde@gmail.com>
2024-10-29 18:26:45 +0000

    cffff

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("./config/cors");
-const authRoutes = require("./routes/authRoutes");
-const employeeRoutes = require("./routes/employeeRoutes");
-const errorHandler = require("./middlewares/errorHandler");
+// const authRoutes = require("./routes/authRoutes");
+// const employeeRoutes = require("./routes/employeeRoutes");
+// const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(cors);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/employees", employeeRoutes);
+// // Add more routes as needed
 
-app.use("/api/auth", authRoutes);
-app.use("/api/employees", employeeRoutes);
-// Add more routes as needed
-
-app.use(errorHandler); // Custom error handling middleware
+// app.use(errorHandler); // Custom error handling middleware
 
 module.exports = app;
